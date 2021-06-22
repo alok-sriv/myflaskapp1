1f8585af0215f6fa16b57505f4c8a7a5a77ea2b3
Alok Srivastava <70486168+alok-sriv@users.noreply.github.com>
2021-06-22 18:42:56 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,26 @@
+def articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Article One",
+            "body":  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, nulla eum possimus asperiores facilis nobis dolor minus eos cumque vero amet sequi autem reprehenderit perspiciatis aperiam esse hic molestiae fugit?",
+            "author": "Alok Srivastava",
+            "created_date": "04-01-2021"
+        },
+        {
+            "id": 2,
+            "title": "Article Two",
+            "body":  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, nulla eum possimus asperiores facilis nobis dolor minus eos cumque vero amet sequi autem reprehenderit perspiciatis aperiam esse hic molestiae fugit?",
+            "author": "Alok Srivastava",
+            "created_date": "05-01-2021"
+        },
+        {
+            "id": 3,
+            "title": "Article Three",
+            "body":  "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, nulla eum possimus asperiores facilis nobis dolor minus eos cumque vero amet sequi autem reprehenderit perspiciatis aperiam esse hic molestiae fugit?",
+            "author": "Alok Srivastava",
+            "created_date": "06-01-2021"
+        }       
+    ]
+
+    return articles
